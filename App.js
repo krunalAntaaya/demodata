f342b2f4933210b58256ec25e1b449183104e7fb
krunalAntaaya <krunalantaaya@gmail.com>
2022-03-20 20:00:05 +0530

    smartdemo:data

---
+++ App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+
+const HomeScreen = () => {
+  return (
+    <View style={styles.container}>
+      <View>
+        <TouchableOpacity>
+          <Text>Submit </Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    // justifyContent: 'center',
+    // alignItems: 'center',
+    // backgroundColor: '#2c3e50',
+  },
+});
+
+export default HomeScreen;
